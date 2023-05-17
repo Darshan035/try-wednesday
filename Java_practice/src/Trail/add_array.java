119219d8b821d05d7f041fb186cee288ed1fe13a
User <User@DESKTOP-5MLG6JB>
2023-05-17 10:02:35 +0530

    modified

---
+++ Java_practice/src/Trail/add_array.java
@@ -0,0 +1,19 @@
+package Trail;
+
+public class add_array {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,50};
+		int sum=0;
+		
+		for(int i=0;i<a.length;i++) {
+		sum=sum+a[i];
+			                  //  System.out.println(a[i]);  // it will print all array
+		}
+		
+		System.out.println(sum);
+		
+
+	}
+
+}
